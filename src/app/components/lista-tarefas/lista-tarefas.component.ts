d64b14947d26ebadcf1e8093cc81c65dc8c13d56
Matheus Augusto <123843457+maugsilva@users.noreply.github.com>
2023-11-25 21:18:01 -0300

    Add files via upload

---
+++ src/app/components/lista-tarefas/lista-tarefas.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Tarefa } from 'src/app/models/tarefa';
+import { TarefaService } from 'src/app/services/tarefa.service';
+
+@Component({
+  selector: 'app-lista-tarefas',
+  templateUrl: './lista-tarefas.component.html',
+  styleUrls: ['./lista-tarefas.component.css']
+})
+export class ListaTarefasComponent {
+  tarefas: Tarefa[] = [];
+  constructor(private tarefaService: TarefaService) {}
+
+  ngOnInit(): void {
+    this.tarefaService.getTarefas().subscribe({
+      next: (tarefas) => {
+        this.tarefas = tarefas;
+        console.log(this.tarefas);
+      }
+    })
+  }
+
+  deletar(id: number) {
+    this.tarefaService.deletarTarefa(id).subscribe({
+      next: () => {
+        this.tarefaService.getTarefas().subscribe((tarefas) => (this.tarefas = tarefas));
+      },
+    });
+  }
+}
